9f7c04ee0ddb12900187e5c6c764e9b67e3d3ef1
Joonhak <ofnullable@gmail.com>
2019-08-24 06:17:29 +0900

    Fix class name

---
+++ src/main/java/org/slam/dto/account/RoleDto.java
@@ -0,0 +1,12 @@
+package org.slam.dto.account;
+
+import lombok.*;
+
+@Getter @Setter @ToString
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class RoleDto {
+
+    private Long   id;
+    private String name;
+
+}
