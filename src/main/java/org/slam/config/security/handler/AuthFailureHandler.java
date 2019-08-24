1b98a31d440a927898682f8d6ac9edccd33396c2
Joonhak <ofnullable@gmail.com>
2019-08-25 04:40:42 +0900

    Fix security handlers

--- src/main/java/org/slam/config/security/handler/AuthFailureHandler.java
+++ src/main/java/org/slam/config/security/handler/AuthFailureHandler.java
@@ -1,8 +1,11 @@
 package org.slam.config.security.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slam.error.ApiError;
+import org.slam.error.ErrorCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
@@ -13,12 +16,24 @@ import java.io.IOException;
 
 public class AuthFailureHandler implements AuthenticationFailureHandler {
 
+    private ObjectMapper mapper = new ObjectMapper();
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Override
-    public void onAuthenticationFailure(HttpServletRequest req, HttpServletResponse res, AuthenticationException ex) throws IOException, ServletException {
-        log.warn("Authentication Failure: {}", ex.getMessage());
-        res.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+    public void onAuthenticationFailure(HttpServletRequest req, HttpServletResponse res, AuthenticationException e) throws IOException, ServletException {
+        log.debug("authentication failure: {}", e.getMessage());
+
+        final var errorCode = ErrorCode.BAD_CREDENTIALS;
+        final var error = ApiError.builder()
+                .status(errorCode.getStatus())
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        res.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        res.getWriter().write(mapper.writeValueAsString(error));
+        res.flushBuffer();
     }
 
 }
