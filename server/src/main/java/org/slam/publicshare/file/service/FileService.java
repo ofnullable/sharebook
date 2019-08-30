99af08b2713024e65a824a3c1b0e0ae71c0d4b3f
Joonhak <ofnullable@gmail.com>
2019-08-30 22:08:49 +0900

    Add file package

--- server/src/main/java/org/slam/publicshare/file/service/FileService.java
+++ server/src/main/java/org/slam/publicshare/file/service/FileService.java
@@ -1,8 +1,27 @@
-package org.slam.publicshare.service.common;
+package org.slam.publicshare.file.service;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.net.ftp.FTPFile;
+import org.slam.publicshare.file.utils.FtpUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.integration.file.remote.session.SessionFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class FileService {
 
-    /*
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     private final SessionFactory<FTPFile> sf;
     private static final String INITIAL_PATH = "share/book/";
 
@@ -45,25 +64,14 @@ public class FileService {
         Arrays.stream(splitPath).forEach(p -> {
             try (var session = sf.getSession()) {
                 log.info("mkdir : {}", temp);
-                session.mkdir(temp.append("/").append(p).toString());
+                if (!p.equals(" ") || !StringUtils.isEmpty(p)) {
+                    session.mkdir(temp.append("/").append(p).toString());
+                }
             } catch (IOException e) {
                 log.error("Can not make directory. Path : {}", temp);
                 e.printStackTrace();
             }
         });
     }
-
-    private boolean isDirExist(String filePath) { // if dir not exist, exception occurred
-        try ( var session = sf.getSession() ) {
-            boolean isExists = session.exists(filePath);
-            log.info("Is exists? Path : {}, result : {}", filePath, isExists);
-            return isExists;
-        } catch (IOException e) {
-            log.error("Can not check is exists. Path : {}", filePath);
-            e.printStackTrace();
-            return false;
-        }
-    }
-    */
 
 }
