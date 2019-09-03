7a8b73daad3bcff1fedb24749f3900895cabb937
Joonhak <ofnullable@gmail.com>
2019-09-04 06:18:48 +0900

    Add logout success handler, get current account function

--- server/src/main/java/org/slam/publicshare/config/WebMvcConfig.java
+++ server/src/main/java/org/slam/publicshare/config/WebMvcConfig.java
@@ -3,7 +3,6 @@ package org.slam.publicshare.config;
 import lombok.RequiredArgsConstructor;
 import org.slam.publicshare.interceptor.NoticeInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
