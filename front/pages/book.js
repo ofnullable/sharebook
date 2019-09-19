4b4deb163ea3650b7a90bd01daf3d5b06bbb64b0
Joonhak <ofnullable@gmail.com>
2019-09-19 19:37:05 +0900

    Fix bugs, Add consts.js

--- front/pages/book.js
+++ front/pages/book.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import { loadBookRequest } from '@redux/actions/bookActions';
+import { loadRentalInfoByBookIdRequest } from '@redux/actions/rentalActions';
 import BookDetail from '@components/BookDetail';
 
 const Book = () => {
@@ -14,10 +15,11 @@ const Book = () => {
   );
 };
 
-Book.getInitialProps = async ctx => {
-  const bookId = ctx.query.id;
-  ctx.store.dispatch(loadBookRequest(bookId));
-  return { id: bookId };
+Book.getInitialProps = async ({ query, store }) => {
+  const bookId = query.id;
+
+  store.dispatch(loadBookRequest(bookId));
+  store.dispatch(loadRentalInfoByBookIdRequest(bookId));
 };
 
 export default Book;
