dced694a8a1b6e49e6836bfe7d6c64e71b0658a2
Joonhak <ofnullable@gmail.com>
2019-09-19 19:25:13 +0900

    Fix nav style for add header menu

--- front/components/layout/nav.styled.js
+++ front/components/layout/nav.styled.js
@@ -5,7 +5,7 @@ import { COLOR_SCHEME } from '@styles/colors';
 
 export const HeaderNav = styled.nav`
   text-align: center;
-  & ul {
+  & > ul {
     ${device.laptops`
       padding: 15px;
     `}
@@ -16,22 +16,49 @@ export const HeaderNav = styled.nav`
       padding: 10px;
     `}
     margin: 0;
-    display: flex;
-    justify-content: space-between;
+    width: 100%;
+    display: inline-block;
     background-color: ${COLOR_SCHEME.primary};
-    & li {
-      color: ${COLOR_SCHEME.white};
-      display: flex;
-      padding: 6px 8px;
-      align-items: center;
-    }
+  }
+`;
+
+export const HeaderMenu = styled.li`
+  color: ${COLOR_SCHEME.white};
+  display: inline-block;
+  padding: 6px 8px;
+  &:last-child {
+    float: right;
     & a {
-      color: inherit;
-      vertical-align: middle;
+      ${device.laptops`
+        line-height: 1.5;
+      `}
+      ${device.tablets`
+        line-height: 1.3;
+      `}
     }
-    & div {
-      display: flex;
-    }
+  }
+  &:first-child {
+    float: left;
+  }
+  & a {
+    display: inline-block;
+    vertical-align: middle;
+    color: inherit;
+  }
+`;
+
+export const HeaderMenuGroup = styled.div`
+  padding: 0;
+  display: inline-block;
+  vertical-align: middle;
+  & span {
+    padding: 0 10px;
+    ${device.laptops`
+      line-height: 1.5;
+    `}
+    ${device.tablets`
+      line-height: 1.3;
+    `}
   }
 `;
 
@@ -50,7 +77,7 @@ export const HomepageLink = styled.a`
   `}
   ${device.tablets`
     & span, & i {
-      font-size: 1.2em;
+      font-size: 20px;
     }
     & span {
       vertical-align: middle;
@@ -62,11 +89,11 @@ export const HomepageLink = styled.a`
   `}
   ${device.mobiles`
     & span {
-      display: none;
+      font-size: 18px;
+      vertical-align: middle;
     }
     & i {
-      font-size: 1.5em;
-      vertical-align: middle;
+      display: none;
     }
   `}
   color: white;
