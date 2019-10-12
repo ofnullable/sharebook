97e8031c185c66c87940dddf2e2a3baed25edb64
Joonhak <ofnullable@gmail.com>
2019-10-12 21:37:16 +0900

    :construction: Add load rental list action, api ...

--- front/redux/api/rental.js
+++ front/redux/api/rental.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-export const loadMyRentalInfoApi = () => {
-  return axios.get(`/account/rental`, { withCredentials: true });
-};
-
-export const loadRentalInfoByBookIdApi = bookId => {
-  return axios.get(`book/${bookId}/rental/`, { withCredentials: true });
-};
-
 export const rentalBookApi = bookId => {
   return axios.post(`/book/${bookId}/rental`, {}, { withCredentials: true });
+};
+
+export const loadRentalListApi = ({ status, page, size }) => {
+  return axios.get(`/account/rentals/${status}?page=${page}&size=${size}`, {
+    withCredentials: true,
+  });
 };
