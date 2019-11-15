e37b861718c464464db0b0e0f9ee0ab99a162fe4
ofnullable <ofnullable@gmail.com>
2019-11-16 05:27:30 +0900

    :construction: Add and fix redux actions for check email duplicate, sign in after sign up success

--- front/redux/reducers/user.js
+++ front/redux/reducers/user.js
@@ -8,17 +8,38 @@ const initial = {
     isSignedIn: false,
     isLoading: false,
     error: {},
+    signInError: false,
   },
   join: {
-    data: {},
     error: {},
     isLoading: false,
+    isDuplicated: true,
   },
 };
 
 export default (state = initial, action) => {
   return produce(state, draft => {
     switch (action.type) {
+      case USER.EMAIL_DUPLICATION_CHECK_REQUEST:
+      case USER.SIGN_UP_REQUEST:
+        draft.join.error = {};
+        draft.join.isLoading = true;
+        break;
+      case USER.EMAIL_DUPLICATION_CHECK_SUCCESS:
+        draft.join.isDuplicated = action.data;
+        draft.join.isLoading = false;
+        break;
+      case USER.SIGN_UP_SUCCESS:
+        draft.user.data = action.data;
+        draft.user.isSignedIn = true;
+        draft.join.isLoading = false;
+        break;
+      case USER.EMAIL_DUPLICATION_CHECK_FAILURE:
+      case USER.SIGN_UP_FAILURE:
+        draft.join.error = action.error;
+        draft.join.isLoading = false;
+        break;
+
       case USER.SIGN_IN_REQUEST:
       case USER.SIGN_OUT_REQUEST:
       case USER.LOAD_USER_REQUEST:
@@ -40,27 +61,18 @@ export default (state = initial, action) => {
 
       case USER.SIGN_IN_FAILURE:
       case USER.LOAD_USER_FAILURE:
-        draft.user.error = action.error;
-        draft.user.isSignedIn = false;
+        if (action.error.status === 401) {
+          draft.user.error = action.error;
+        } else {
+          draft.user.signInError = true;
+        }
         draft.user.data = {};
+        draft.user.isSignedIn = false;
         draft.user.isLoading = false;
         break;
       case USER.SIGN_OUT_FAILURE:
         draft.user.error = action.error;
         draft.user.isLoading = false;
-        break;
-
-      case USER.SIGN_UP_REQUEST:
-        draft.join.error = {};
-        draft.join.isLoading = true;
-        break;
-      case USER.SIGN_UP_SUCCESS:
-        draft.join.data = action.data;
-        draft.join.isLoading = false;
-        break;
-      case USER.SIGN_UP_FAILURE:
-        draft.join.error = action.error;
-        draft.join.isLoading = false;
         break;
 
       default:
