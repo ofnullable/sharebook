253b060d4e0e151deae3af0dc0a482e3c37e1d1c
ofnullable <ofnullable@gmail.com>
2019-11-29 17:35:39 +0900

    :construction: Move files and separate project api with file service

---
+++ server/module-api/src/main/java/me/ofnullable/sharebook/config/security/filter/RestAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package me.ofnullable.sharebook.config.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.ofnullable.sharebook.config.security.userdetails.SignInRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import static java.util.stream.Collectors.joining;
+
+public class RestAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private String username;
+    private String password;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public RestAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super.setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException {
+        var authToken = readAndMakeToken(req);
+
+        // Allow subclasses to set the "details" property
+        setDetails(req, authToken);
+
+        return this.getAuthenticationManager().authenticate(authToken);
+    }
+
+    private UsernamePasswordAuthenticationToken readAndMakeToken(HttpServletRequest req) {
+        readBody(req);
+        return new UsernamePasswordAuthenticationToken(username, password);
+    }
+
+    private void readBody(HttpServletRequest req) {
+        try (BufferedReader reader = req.getReader()) {
+            var stringified = reader.lines().collect(joining());
+            var body = mapper.readValue(stringified, SignInRequest.class);
+            setFields(body);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setFields(SignInRequest body) {
+        this.username = body.getUsername() == null ? "" : body.getUsername();
+        this.password = body.getPassword() == null ? "" : body.getPassword();
+    }
+
+}
---
+++ server/module-file-service/src/test/java/me/ofnullable/file/utils/StorageUtilsTest.java
@@ -0,0 +1,34 @@
+package me.ofnullable.file.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StorageUtilsTest {
+
+    @Test
+    @DisplayName("현재 날짜로 폴더명 생성")
+    void makeDirectoryName() {
+        var directory = StorageUtils.makeDirectoryName();
+
+        var format = DateTimeFormatter.ofPattern("/YYYYMMdd");
+        assertEquals(directory, LocalDate.now().format(format));
+    }
+
+    @Test
+    @DisplayName("현재 시간과 파일명으로 고유한 파일명 생성")
+    void makeUniqueFilename() {
+        var filename = StorageUtils.makeUniqueFilename("test.jpg");
+
+        var format = DateTimeFormatter.ofPattern("/HHmm");
+        assertTrue(filename.startsWith(LocalDateTime.now().format(format)));
+        assertTrue(filename.endsWith("test.jpg"));
+    }
+
+}
