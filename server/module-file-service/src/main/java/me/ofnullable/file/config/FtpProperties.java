6606393df19a398a46d1f5c58d1ef68ea465ae4b
ofnullable <ofnullable@gmail.com>
2019-11-30 15:08:32 +0900

    :bug: Add ftp properties constraint, set for ftp default base path

--- server/module-file-service/src/main/java/me/ofnullable/file/config/FtpProperties.java
+++ server/module-file-service/src/main/java/me/ofnullable/file/config/FtpProperties.java
@@ -4,13 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter @Setter
 @ConfigurationProperties(prefix = "sharebook.ftp")
 public class FtpProperties {
 
-    private int port;
+    @NotNull
+    private Integer port;
+    @NotBlank
     private String host;
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
     private String basePath;
 
